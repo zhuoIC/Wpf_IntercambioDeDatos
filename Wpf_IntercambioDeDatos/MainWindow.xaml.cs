240c3142cae38931b36bc65abaafdd3d3964a74f
zhuoIC <nicolimonar@gmail.com>
2017-04-20 10:42:21 +0200

    Hecho en clase

---
+++ Wpf_IntercambioDeDatos/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_IntercambioDeDatos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            App objActual = Application.Current as App; // as devuelve null a diferencia del casting que salta la excepcion
+            objActual.Dato = tbxOrigen.Text;
+            if (objActual != null)
+            {
+                VenDestino ventana = new VenDestino();
+                ventana.Show();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown(); // Cierra todas las ventanas de la aplicación
+        }
+    }
+}
---
+++ Wpf_IntercambioDeDatos/venDestino.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_IntercambioDeDatos
+{
+    /// <summary>
+    /// Lógica de interacción para verDestino.xaml
+    /// </summary>
+    public partial class VenDestino : Window
+    {
+        public VenDestino()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            tbxDestino.Text = (Application.Current as App).Dato;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            tbxDestino.Text = (Application.Current as App).Dato;
+        }
+    }
+}
